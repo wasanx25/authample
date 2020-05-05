cf4f9468ebb230928af62fbd1d11048e947883e7
wasanx25 <wasanx25@gmail.com>
2020-05-05 14:48:39 +0900

    Add filters test

--- tests/unit/PlayerTable.spec.ts
+++ tests/unit/PlayerTable.spec.ts
@@ -2,16 +2,46 @@ import { shallowMount } from '@vue/test-utils'
 import PlayerTable from '@/components/PlayerTable.vue'
 
 describe('PlayerTable.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders default state', () => {
     // setup
     const wrapper = shallowMount(PlayerTable)
 
-    // expect default
-    expect(wrapper.vm.$data.filterWord).toMatch('')
+    // when~then
+    expect(wrapper.vm.$data.filterWord).toBe('')
     expect(wrapper.find('table').exists()).toBe(true)
+    expect(wrapper.find('p').exists()).toBe(true)
+  })
 
-    // type text
-    wrapper.find('input[type="text"]').setValue('Fue')
-    expect(wrapper.vm.$data.filterWord).toMatch('Fue')
+  it('filters players', () => {
+    // setup
+    const wrapper = shallowMount(PlayerTable)
+    wrapper.setData({
+      players: [
+        {
+          name: 'Fue',
+          age: '20',
+          salary: '20000000'
+        },
+        {
+          name: 'Hoe',
+          age: '31',
+          salary: '1000000000'
+        },
+        {
+          name: 'Boo',
+          age: '40',
+          salary: '50000000000'
+        }
+      ]
+    })
+
+    // when~then
+    wrapper.find('input[type="text"]').setValue('Fu')
+    expect(wrapper.vm.$data.filterWord).toBe('Fu')
+    expect(wrapper.vm.$data.filterPlayers).toStrictEqual([{ age: '20', name: 'Fue', salary: '20000000' }])
+
+    wrapper.find('input[type="text"]').setValue('Hoe')
+    expect(wrapper.vm.$data.filterWord).toBe('Hoe')
+    expect(wrapper.vm.$data.filterPlayers).toStrictEqual([{ age: '31', name: 'Hoe', salary: '1000000000' }])
   })
 })
