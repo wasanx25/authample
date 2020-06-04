31f6bc370dd39dd524243e18397bd1fc42fd960f
wasanx25 <wasanx25@gmail.com>
2020-06-05 00:04:45 +0900

    Add CORS mapping configuration from frontend

---
+++ backend/src/main/kotlin/com/github/wasanx25/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.github.wasanx25
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8090")
+                .allowedMethods("GET", "POST")
+    }
+}
