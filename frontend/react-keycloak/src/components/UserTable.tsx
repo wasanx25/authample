ba6c5d59fed26333913e800e48d59259adaff20e
wasanx25 <wasanx25@gmail.com>
2020-06-06 23:37:08 +0900

    Add component sample

---
+++ frontend/react-keycloak/src/components/UserTable.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function UserTable () {
+  return (
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Salary</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Fue</td>
+          <td>29</td>
+          <td>100000</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default UserTable
