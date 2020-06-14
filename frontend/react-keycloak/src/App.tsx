51dcb762c9e2625f514e151e69ae68afd1f6276e
wasanx25 <wasanx25@gmail.com>
2020-06-14 18:39:10 +0900

    Add react-router

--- frontend/react-keycloak/src/App.tsx
+++ frontend/react-keycloak/src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
 import UserTable from './components/UserTable'
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <UserTable/>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about' >About</Link>
+        </li>
+        <li>
+          <Link to='/users' >Users</Link>
+        </li>
+      </ul>
+
+      <hr/>
+
+      <Switch>
+        <Route exact path='/'>
+          <div>
+            <h2>Hello Home</h2>
+          </div>
+        </Route>
+        <Route path='/about'>
+          <div>
+            <h2>About</h2>
+          </div>
+        </Route>
+        <Route path='/users'>
+          <div>
+            <UserTable/>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
