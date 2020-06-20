b4d0647a83a24624d69dcbb2babacf0eb271e9e4
wasanx25 <wasanx25@gmail.com>
2020-06-21 01:08:08 +0900

    Add keycloak-js and copy initialize code from vue-keycloak

--- frontend/react-keycloak/src/index.tsx
+++ frontend/react-keycloak/src/index.tsx
@@ -5,13 +5,46 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import Keycloak from 'keycloak-js'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+const options = {
+  url: 'http://localhost:8081/auth',
+  realm: 'SampleAuth',
+  clientId: 'login-app'
+}
+
+const keycloak = Keycloak(options)
+
+keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
+  if (authenticated) {
+    console.log('Authenticated')
+  } else {
+    console.log('Not Authenticated')
+    window.location.reload()
+  }
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+
+  setInterval(() => {
+    keycloak.updateToken(30).then((refreshed) => {
+      if (refreshed) {
+        console.log('Token refreshed')
+      } else {
+        console.warn('Token not refreshed, valid for ')
+      }
+    }).catch((error) => {
+      console.error('Failed to refresh token: ' + error)
+    })
+  }, 60 * 1000)
+}).catch((error) => {
+  console.log('Authenticated Failed: ' + error)
+})
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
