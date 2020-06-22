1eb7877f408b9a6199cb6b9fdcd490c1af60fba9
wasanx25 <wasanx25@gmail.com>
2020-06-23 03:19:16 +0900

    Refactor App.test

--- frontend/react-keycloak/src/__tests__/App.test.tsx
+++ frontend/react-keycloak/src/__tests__/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from '../App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders routing link list', () => {
+  const result = render(<App />)
+  const expectedHTML = `
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/users">Users</a></li>
+      <li><a href="/cards">Cards</a></li>
+    </ul>
+  `.replace(/\s*</gi, '\<').trimEnd()
+
+  expect(result.container.innerHTML).toContain(expectedHTML)
+})
