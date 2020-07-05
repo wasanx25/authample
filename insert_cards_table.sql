b59315634dc316141ffe71097f58afbb95916f18
wasanx25 <wasanx25@gmail.com>
2020-07-05 13:06:25 +0900

    Fix insert statement to SERIAL column

--- insert_cards_table.sql
+++ insert_cards_table.sql
@@ -1,5 +1,5 @@
-INSERT INTO cards (id, title, description, status) VALUES
-    (1, 'sample 1', 'this card is sample', 'todo'),
-    (2, 'sample 2', 'this card doing', 'doing'),
-    (3, 'sample 3', 'this card is done. do not touch this', 'done');
+INSERT INTO cards (title, description, status) VALUES
+    ('sample 1', 'this card is sample', 'todo'),
+    ('sample 2', 'this card doing', 'doing'),
+    ('sample 3', 'this card is done. do not touch this', 'done');
 
